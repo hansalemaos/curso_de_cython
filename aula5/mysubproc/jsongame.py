6319db8f83a60dd0f20a80fa55f94089bae70263
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2025-03-27 14:03:23 -0300

    Delete jsongame.py

--- aula5/mysubproc/jsongame.py
+++
@@ -1,9 +0,0 @@
-import json
-import pickle
-
-with open(r"C:\mypathforfiles3\1742313898.6383076.pkl", "rb") as f:
-    data = pickle.load(f)
-
-
-with open(r"C:\mypathforfiles3\1742313898.6383076.json", "w") as f:
-    json.dump(data, f)
